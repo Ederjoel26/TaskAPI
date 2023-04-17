191b8b9834c954c3087253e1390edffcff47e88e
Ederjoel26 <eder_1321124031@uptecamac.edu.mx>
2023-04-16 21:01:01 -0600

    First commit

---
+++ dist/config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.config = void 0;
+var config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "tasksdb"
+};
+exports.config = config;
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
 "use strict";
 
-console.log('Hello World');
+var _app = _interopRequireDefault(require("./app"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+_app["default"].listen(3000);
+console.log("Server on port 3000");
